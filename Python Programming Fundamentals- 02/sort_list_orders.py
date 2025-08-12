3371c6549480061fc7eaff1d125c9becd233bb7f
Sabbir Hossain Durjoy <hossain15-4724@diu.edu.bd>
2025-08-13 00:07:04 +0600

    Add files via upload

---
+++ Python Programming Fundamentals- 02/sort_list_orders.py
@@ -0,0 +1,8 @@
+def sort_function():
+    Li = [1, 5, 2, 9, 3, 22, 13]
+    Li.sort()
+    print('Ascending order:', Li)
+    Li.sort(reverse=True)
+    print('Descending order:', Li)
+
+sort_function()
